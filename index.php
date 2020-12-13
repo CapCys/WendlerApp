dd47c9f58af95053b98ad01be6bc3d618a900285
JaysCoachD <theeckert4@gmail.com>
2020-12-13 09:22:02 -0600

    create Timer with Start/Pause and Reset

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>My Wendler 5/3/1 Workout</title>
+			<meta name="title" content="My Wendler 5/3/1 Workout" />
+			<meta name="keywords" content="" />
+			<meta name="description" content="Today's Wendler Workout" />
+			<meta http-equiv="content-type" content="text/html"/>
+			<meta name="robots" content="index,follow" />
+			<meta name="googlebot" content="index,follow" />
+			<meta name="msnbot" content="index,follow" />
+			<meta name="rating" content="GENERAL" />
+			<meta name="distribution" content="GLOBAL" />
+			<meta name="author" content="CapCys Web Development" />
+			<meta name="revisit-after" content="7 Days" />
+	    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+		  <link rel="icon" href="favicon.ico" type="image/x-icon">
+  </head>
+  <body>
+    <div id="timeDisplay">00:00</div>
+    <div>
+      <button id="startingPistol">Start</button>
+      <button id="reset">Reset</button>
+    </div>
+    <script src="js/timer.js"></script>
+  </body>
+</html>
---
+++ js/timer.js
@@ -0,0 +1,52 @@
+function stopTimer(isRunning, button, buttonText){
+    clearInterval(isRunning);
+    button.innerHTML = buttonText;
+  }
+  
+  function getTimeRemaining(remaining) {
+    const seconds = Math.floor(remaining % 60);
+    const minutes = Math.floor((remaining / 60) % 60);
+    
+    return {
+      minutes,
+      seconds
+    };
+  }
+  
+  function updateClock(remaining) {
+    var title = document.getElementById("timeDisplay");
+    const t = getTimeRemaining(remaining);
+  
+    title.innerHTML = ('0' + t.minutes).slice(-2) + ":" + ('0' + t.seconds).slice(-2);
+  
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+  
+  var startButton = document.getElementById("startingPistol");
+  var resetButton = document.getElementById("reset");
+  var time = 122;
+  var isRunning = -1;
+  updateClock(time);
+  
+  startButton.addEventListener("click", function(event){
+    if(isRunning == -1){
+      isRunning = setInterval(function(){
+        time--;
+        updateClock(time);
+      }, 1000);
+      startButton.innerHTML = "Pause";
+    } else {
+      stopTimer(isRunning, startButton, "Resume");
+      isRunning = -1;
+    }
+  })
+  
+  resetButton.addEventListener("click", function(event){
+    stopTimer(isRunning, startButton, "Start");
+    isRunning = -1;
+    time = 122;
+    updateClock(time);
+  })
+  ;
