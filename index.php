4713c8033d2562d12f88d4b0c37e8d9bf02e63df
Damon Eckert <theeckert4@gmail.com>
2020-12-19 14:14:11 -0600

    add bootstrap and create homepage (#2)

--- index.php
+++ index.php
@@ -1,37 +1,115 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html lang="en">
-	<head>
-	    <meta charset="utf-8">
-	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<title>My Wendler 5/3/1 Workout</title>
-			<meta name="title" content="My Wendler 5/3/1 Workout" />
-			<meta name="keywords" content="" />
-			<meta name="description" content="Today's Wendler Workout" />
-			<meta http-equiv="content-type" content="text/html"/>
-			<meta name="robots" content="index,follow" />
-			<meta name="googlebot" content="index,follow" />
-			<meta name="msnbot" content="index,follow" />
-			<meta name="rating" content="GENERAL" />
-			<meta name="distribution" content="GLOBAL" />
-			<meta name="author" content="CapCys Web Development" />
-			<meta name="revisit-after" content="7 Days" />
-	    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-		  <link rel="icon" href="favicon.ico" type="image/x-icon">
-	    <link rel="stylesheet" type="text/css" href="css/timer.css">
-  </head>
-  <body>
-    <div>
-      <input type="radio" id="easy" name="defaultTime" value="Easy" checked="true">
-      <label for="easy">Easy</label>
-      <input type="radio" id="hard" name="defaultTime" value="Hard">
-      <label for="hard">Hard</label>
-      <button id="applyDefaultTime" onclick="applyDefault()">Apply</button>
-    </div><br />
-    <div id="timeDisplay">00:00</div><br />
-    <div>
-      <button id="startingPistol">Start</button>
-      <button id="reset">Reset</button>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CapCys</title>
+  <meta name="title" content="My Wendler 5/3/1 Workout" />
+  <meta name="keywords" content="" />
+  <meta name="description" content="Today's Wendler Workout" />
+  <meta http-equiv="content-type" content="text/html"/>
+  <meta name="robots" content="index,follow" />
+  <meta name="googlebot" content="index,follow" />
+  <meta name="msnbot" content="index,follow" />
+  <meta name="rating" content="GENERAL" />
+  <meta name="distribution" content="GLOBAL" />
+  <meta name="author" content="CapCys Web Development" />
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <link rel="icon" href="favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+  body {
+    font: 20px Montserrat, sans-serif;
+    line-height: 1.8;
+    color: #f5f6f7;
+    background-color: #f5f6f7;
+  }
+  p {font-size: 16px;}
+  .margin {margin-bottom: 45px;}
+  .bg-1 { 
+    background-color: #337ab7; /* Green #1abc9c*/
+    color: #ffffff;
+  }
+  .bg-2 {
+    color: #f5f6f7;
+  }
+  .footer-copyright {
+    color:#337ab7;
+    background-color: #f5f6f7;
+  }
+  .list-group-item {
+    background-color: #337ab7;
+    border: #337ab7;
+  }
+  .container-fluid {
+    padding-top: 120px;
+    padding-bottom: 120px;
+  }
+  .navbar {
+    padding-top: 15px;
+    padding-bottom: 15px;
+    border: 0;
+    border-radius: 0;
+    margin-bottom: 0;
+    font-size: 12px;
+    letter-spacing: 5px;
+  }
+  .navbar-nav  li a:hover {
+    color: #1abc9c !important;
+  }
+  </style>
+</head>
+<body>
+
+<!-- Navbar -->
+<nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
     </div>
-    <script src="js/timer.js"></script>
-  </body>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <!-- <li><a href="#">About</a></li>
+        <li><a href="workoutStep.php">Workout</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li> -->
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- First Container -->
+<div class="container-fluid bg-1">
+  <div class="col-sm-6 text-center">
+    <h3 class="margin">Who Am I?</h3>
+    <img src="images/canoelogo.jpg" class="img-responsive img-circle margin" style="display:inline" alt="Portage Man" width="150" height="150">
+  </div>
+  <div class="col-sm-6 text-center">
+    <ul class="list-group">
+      <li class="list-group-item bg-1">He/Him/His</li>
+      <li class="list-group-item bg-1">Outdoor Enthusiast</li>
+      <li class="list-group-item bg-1">Strong Curiosity</li>
+      <li class="list-group-item bg-1">Foodie</li>
+      <li class="list-group-item bg-1">Software Developer</li>
+    </ul>
+  </div>
+</div>
+
+<!-- Footer -->
+<footer class="container text-center">
+  <p class="footer-copyright text-center">Copyright CapCys Web Development</p>
+</footer>
+
+</body>
 </html>
